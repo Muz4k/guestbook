45b55056b53f01f8d08e60626d6bba1e4a8e76e5
Muz4k <muz4k.oo@gmail.com>
2020-06-13 19:40:25 +0300

    added show

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,25 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
     /**
+     * @param Environment $twig
+     * @param ConferenceRepository $conferenceRepository
+     *
+     * @return Response
+     *
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     *
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {
-        return new Response(<<<EOF
-        <html>
-        <body>
-        <img src="/images/under-construction.gif" />
-        </body>
-        </html>
-EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
+
+    /**
+     * @param Environment $twig
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     *
+     * @return Response
+     *
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     *
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' =>
+                $conference], ['createdAt' => 'DESC']),
+        ]));
+ }
 }
